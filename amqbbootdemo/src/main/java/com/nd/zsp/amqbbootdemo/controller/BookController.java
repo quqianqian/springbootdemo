6278d92e6bb9c29add482038f140f5c13c3b544f
郑少朋 <911917@nd>
2018-06-05 11:39:09 +0800

    delay message queue by rabbit

--- amqbbootdemo/src/main/java/com/nd/zsp/amqbbootdemo/controller/BookController.java
+++ amqbbootdemo/src/main/java/com/nd/zsp/amqbbootdemo/controller/BookController.java
@@ -2,11 +2,16 @@ package com.nd.zsp.amqbbootdemo.controller;
 
 import com.nd.zsp.amqbbootdemo.config.RabbitConfig;
 import com.nd.zsp.amqbbootdemo.domain.Book;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.AbstractJavaTypeMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
 
 /**
  * 消息发送
@@ -14,6 +19,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/books")
 public class BookController {
+
+    private static final Logger log = LoggerFactory.getLogger(BookController.class);
 
     private final RabbitTemplate rabbitTemplate;
 
@@ -33,5 +40,21 @@ public class BookController {
         book.setName("雪雪大大是打发斯蒂芬");
         this.rabbitTemplate.convertAndSend(RabbitConfig.DEFAULT_BOOK_QUEUE, book);
         this.rabbitTemplate.convertAndSend(RabbitConfig.MANUAL_BOOK_QUEUE, book);
+    }
+
+    @GetMapping("/delay")
+    public void delayMessage() {
+        Book book = new Book();
+        book.setId(2000L);
+        book.setName("雪雪大大是打发斯蒂芬");
+        // 添加延时队列
+        this.rabbitTemplate.convertAndSend(RabbitConfig.REGISTER_DELAY_EXCHANGE, RabbitConfig.DELAY_ROUTING_KEY, book, message -> {
+            // TODO 第一句是可要可不要,根据自己需要自行处理
+            message.getMessageProperties().setHeader(AbstractJavaTypeMapper.DEFAULT_CONTENT_CLASSID_FIELD_NAME, Book.class.getName());
+            // TODO 如果配置了 params.put("x-message-ttl", 5 * 1000); 那么这一句也可以省略,具体根据业务需要是声明 Queue 的时候就指定好延迟时间还是在发送自己控制时间
+            message.getMessageProperties().setExpiration(5 * 1000 + "");
+            return message;
+        });
+        log.info("[发送时间] - [{}]", LocalDateTime.now());
     }
 }
--- amqbbootdemo/src/main/java/com/nd/zsp/amqbbootdemo/handler/BookHandler.java
+++ amqbbootdemo/src/main/java/com/nd/zsp/amqbbootdemo/handler/BookHandler.java
@@ -10,6 +10,7 @@ import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 
 /**
  * BOOK_QUEUE 消费者
@@ -51,6 +52,17 @@ public class BookHandler {
     @RabbitListener(queues = {RabbitConfig.MANUAL_BOOK_QUEUE})
     public void listenerManualAck(Book book, Message message, Channel channel) {
         log.info("[listenerManualAck 监听的消息] - [{}]", book.toString());
+        try {
+            // TODO 通知 MQ 消息已被成功消费,可以ACK了
+            channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
+        } catch (IOException e) {
+            // TODO 如果报错了,那么我们可以进行容错处理,比如转移当前消息进入其它队列
+        }
+    }
+
+    @RabbitListener(queues = {RabbitConfig.REGISTER_QUEUE_NAME})
+    public void listenerDelayAck(Book book, Message message, Channel channel) {
+        log.info("[listenerDelayQueue 监听的消息] - [消费时间] - [{}] - [{}]", LocalDateTime.now(), book.toString());
         try {
             // TODO 通知 MQ 消息已被成功消费,可以ACK了
             channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
